dbe9199e896928a91c2f1085ddeec5f791131ee4
Rodrigo Lucchesi <rsl@pop-os.localdomain>
2020-07-01 22:49:01 -0300

    adding changes to code, comenting the steps

--- TUTORIAIS-FLASK/AULA_1/Chapter_3/app/forms.py
+++ TUTORIAIS-FLASK/AULA_1/Chapter_3/app/forms.py
@@ -1,9 +1,9 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm    # Importing flask_wtf module
+from wtforms import StringField, PasswordField, BooleanField, SubmitField  # Importing the fields used to create the HTML impus
+from wtforms.validators import DataRequired # Data required is an alert informing a must input on Login form
 
 
-class LoginForm(FlaskForm):
+class LoginForm(FlaskForm):   # It's a class based Form, to created the required fields
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
--- TUTORIAIS-FLASK/AULA_1/Chapter_3/app/routes.py
+++ TUTORIAIS-FLASK/AULA_1/Chapter_3/app/routes.py
@@ -2,14 +2,18 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
-
+# '/' and '/index' route, defined to return a username as a view function response
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Linus Torvald'}
     return render_template('index.html', title='Home', user=user)
 
-
+# '/login' The login route a instance with the class LoginForm is created.
+# If the required fields as fullfilled and the necessary ones loaded as well, the validation
+# is completed, otherwise return to login page.  The flashing system basically makes it possible to record a 
+# message at the end of a request and access it next request and only next request. 
+# This is usually combined with a layout template that does this.
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
